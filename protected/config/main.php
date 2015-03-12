bda0aa319d55c123ea08418bab6f6b8d4a285b74
SlashMan <andzhilin@gmail.com>
2015-03-12 19:30:38 +0400

    Fix

--- protected/config/main.php
+++ protected/config/main.php
@@ -61,7 +61,7 @@ return array(
 		// uncomment the following to use a MySQL database
 
 		'db'=>array(
-			'connectionString' => 'dblib:host=91.203.194.185;dbnamee=monitoring',
+			'connectionString' => 'dblib:host=91.203.194.185;dbname=monitoring',
 			//'emulatePrepare' => true,
 			'username' => 'sa',
 			'password' => 'JokE5001031',
