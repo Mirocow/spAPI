09ba9f27729a8d174d867ac3c46f48d47dfa5f0b
SlashMan <andzhilin@gmail.com>
2015-05-25 12:54:24 +0400

    Fix

--- protected/components/Core.php
+++ protected/components/Core.php
@@ -5,13 +5,24 @@
  * Date: 15.12.2014
  * Time: 0:46
  */
-class Core {
+class Core
+{
     public static function utfEn(&$item, $key)
     {
         $item = mb_convert_encoding($item, "utf-8", "windows-1251");
     }
+
     public static function utfDe(&$item, $key)
     {
         $item = mb_convert_encoding($item, "windows-1251", "utf-8");
+    }
+
+    public static function utfVarEn($item)
+    {
+        return mb_convert_encoding($item, "utf-8", "windows-1251");
+    }
+    public static function utfVarDe($item)
+    {
+        return mb_convert_encoding($item, "windows-1251", "utf-8");
     }
 }
