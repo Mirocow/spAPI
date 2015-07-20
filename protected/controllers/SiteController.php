693cf7375fc9851ae43f21fabe2a4ca321b15310
SlashMan <andzhilin@gmail.com>
2015-07-20 22:24:36 +0400

    Fix

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -196,7 +196,6 @@ class SiteController extends Controller
                 //array_walk_recursive($data, 'Core::utfDe');
                 $claim = new Claim();
                 $claim->attributes = $data['Claim'];
-                die(11212121);
                 $claim->created = new CDbExpression('NOW()');//new CDbExpression('GETDATE()');
                 $claim->guid = $guid; //@todo Проверка на гуид
                 $claim->status = Claim::STATUS_OPEN;
